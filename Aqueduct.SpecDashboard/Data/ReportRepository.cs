40fbb92adf85ce28312e1992b32be8ec76e3ddab
tom <tom@aqueduct.co.uk>
2012-08-13 10:26:06 +0100

    can deal with no xml in zip

--- Aqueduct.SpecDashboard/Data/ReportRepository.cs
+++ Aqueduct.SpecDashboard/Data/ReportRepository.cs
@@ -45,69 +45,7 @@ namespace Aqueduct.SpecDashboard.Data
         {
             var report = new Report(Path.GetFileNameWithoutExtension(path));
             IList<ReportVersion> getVersions =  GetVersions(path);
-            var latest = getVersions.OrderByDescending(x => x.Date).First();
-            var previous = getVersions.OrderByDescending(x => x.Date).ElementAt(1);
             report.Versions = getVersions;
-
-
-   
-            //// Load test results xml from the latest version
-            //var xmlPath = Path.Combine(latest.Path, "TestResult.xml");
-            //var document = LoadXml(xmlPath);
-
-            //var total = document.Root.Attributes().First(x => x.Name == "total").Value;
-            //int noOfTests = int.Parse(total);
-            //report.NumberOfTests = noOfTests;
-
-            //var errors = document.Root.Attributes().First(x => x.Name == "total").Value;
-            //int noOfErrors = int.Parse(errors);
-            //report.Errors = noOfErrors;
-
-            //var failures = document.Root.Attributes().First(x => x.Name == "failures").Value;
-            //int noOfFailures = int.Parse(failures);
-            //report.Failures = noOfFailures;
-
-            //var inconclusive = document.Root.Attributes().First(x => x.Name == "inconclusive").Value;
-            //int noOfInconclusive = int.Parse(inconclusive);
-            //report.Inconclusive = noOfInconclusive;
-
-            //int success = noOfTests - noOfFailures- noOfInconclusive;
-            //report.Success = success;
-
-            //double percentageSuccess = ((double)success/(double)noOfTests)*100;
-            //var percentageSuccessRound = Math.Round(percentageSuccess, 0);
-            //int percentSuccess = Convert.ToInt32(percentageSuccessRound);
-            //report.PercentageSuccess = percentSuccess;
-
-            //var date = document.Root.Attributes().First(x => x.Name == "date").Value;
-            //report.Date = date;
-
-            //var time = document.Root.Attributes().First(x => x.Name == "time").Value;
-            //report.Time = time;
-
-            //double percentageInconclusive = ((double)noOfInconclusive / (double)noOfTests) * 100;
-            //var percentageInconclusiveRound = Math.Round(percentageInconclusive, 0);
-            //int percentInconclusive = Convert.ToInt32(percentageInconclusiveRound);
-            //report.PercentageInconclusive = percentInconclusive;
-
-            //double percentageFailed = ((double)noOfFailures / (double)noOfTests) * 100;
-            //var percentageFailedRound = Math.Round(percentageFailed, 0);
-            //int percentFail = Convert.ToInt32(percentageFailedRound);
-            //report.PercentageFailure = percentFail;
-
-
-            //if(change > 0)
-            //{
-            //    report.PercentageSuccessDifference = 1;
-            //}
-            //else if(change == 0)
-            //{
-            //    report.PercentageSuccessDifference = 0;
-            //}
-            //else
-            //{
-            //    report.PercentageSuccessDifference = -1;
-            //}
 
             return report;
         }
@@ -132,23 +70,27 @@ namespace Aqueduct.SpecDashboard.Data
             //Calucate all the stats 
             // Load test results xml from the latest version
             var xmlPath = Path.Combine(version.Path, "TestResult.xml");
-            var document = LoadXml(xmlPath);
+            if(xmlPath != null)
+            {
+                var document = LoadXml(xmlPath);
 
-            var total = document.Root.Attributes().First(x => x.Name == "total").Value;
-            int noOfTests = int.Parse(total);
-            statistics.NumberOfTests = noOfTests;
+                var total = document.Root.Attributes().First(x => x.Name == "total").Value;
+                int noOfTests = int.Parse(total);
+                statistics.NumberOfTests = noOfTests;
 
-            var failures = document.Root.Attributes().First(x => x.Name == "failures").Value;
-            int noOfFailures = int.Parse(failures);
-            statistics.Failures = noOfFailures;
+                var failures = document.Root.Attributes().First(x => x.Name == "failures").Value;
+                int noOfFailures = int.Parse(failures);
+                statistics.Failures = noOfFailures;
 
-            var inconclusive = document.Root.Attributes().First(x => x.Name == "inconclusive").Value;
-            int noOfInconclusive = int.Parse(inconclusive);
-            statistics.Inconclusive = noOfInconclusive;
+                var inconclusive = document.Root.Attributes().First(x => x.Name == "inconclusive").Value;
+                int noOfInconclusive = int.Parse(inconclusive);
+                statistics.Inconclusive = noOfInconclusive;
 
-            int success = noOfTests - noOfFailures - noOfInconclusive;
-            statistics.Success = success;
+                int success = noOfTests - noOfFailures - noOfInconclusive;
+                statistics.Success = success;
 
+                
+            }
             return statistics;
         }
 
