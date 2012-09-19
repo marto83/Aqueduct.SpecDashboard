bf24e3186a777ae5064e6c20a9c95c5a3f763b5c
Francis <francis@soulrepublic.com>
2012-09-19 16:15:50 +0100

    changing spec to specs in deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -12,7 +12,7 @@ properties {
 	#Deploy to live. Yes, immediately.
 	$config = "Release"
 	$site_unc = "\\aque-sqldev\d$\Websites\Aqueduct\SpecDashboard"
-	$site_url = "http://spec.aquepreview.com"
+	$site_url = "http://specs.aquepreview.com"
 } 
 
 task UnZip {
@@ -31,14 +31,4 @@ task Deploy -depends UnZip {
 	
 	$webclient = New-Object Net.WebClient
 	$webclient.DownloadString($site_url) | Out-Null
-}
-
-task SeleniumTest {
-	$env:testUrl = $site_url #Set environment variable. It's the only way to pass a variable to an NUnit test.
-	$test_dlls = Get-ChildItem $integration_tests_dir\*.Selenium.Tests.dll
-	
-	foreach($dll in $test_dlls) { 
-		$name = $dll.name
-		exec { & $tools_dir\nunit\nunit-console.exe $dll /xml:$integration_tests_dir\TestResult-$name.xml }
-	}
 }
